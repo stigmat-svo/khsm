f10357aefcfbbc36456c8f7c2220deaa498c89f7
stigmat <sheludko.vo@gmail.com>
2020-07-16 18:08:09 +0300

    start

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Billionaire
     config.time_zone = 'Moscow'
 
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    ### Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
