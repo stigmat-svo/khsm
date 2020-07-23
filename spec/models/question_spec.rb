1ef604f8712d625ec3bba7ee9dd9ac38b1c35a69
stigmat <sheludko.vo@gmail.com>
2020-07-23 16:47:57 +0300

    text_uniqness test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -15,6 +15,9 @@ RSpec.describe Question, type: :model do
     it { should validate_presence_of :text }
     it { should validate_presence_of :level }
 
+
+    it { should validate_uniqueness_of :text }
+
     it { should validate_inclusion_of(:level).in_range(0..14) }
 
     it { should allow_value(14).for(:level) }
