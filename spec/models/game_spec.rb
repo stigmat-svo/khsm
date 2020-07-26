1f1dc1c6a1ee329e782df1805d60797403e0be59
stigmat <sheludko.vo@gmail.com>
2020-07-26 15:50:59 +0300

    tests for current_game_question & previous_level

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -105,4 +105,13 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:money)
     end
   end
+
+
+  it ' .current_game_question' do
+    expect(game_w_questions.current_game_question).to eq(game_w_questions.game_questions.first)
+  end
+
+  it ' .previous_level' do
+    expect(game_w_questions.previous_level).to eq(-1)
+  end
 end
